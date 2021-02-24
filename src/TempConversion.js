68753e0fd1b12bc50ec8f2cf35a3fbd8de43d3c3
alicefren <78852401+alicefren@users.noreply.github.com>
2021-02-24 11:44:59 +0000

    Added unit conversion for temperatures

--- src/TempConversion.js
+++ src/TempConversion.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TempConversion(props){
-    return(
-        <div className="temperature">
-        <span className="temp-degrees"></span>
-            <a href="#" className="celsius-link active">
-             {Math.round(props.data.temperature)} °C{" "}
-            </a>
-            |{" "}
-            <a href="#" className="fahrenheit-link"> °F
-            </a>
+    const [unit, setUnit] = useState("celsius");
+
+    function showFahrenheit(event){
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function showCelsius(event){
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    function fahrenheit(){
+        return (props.celsius * 9) / 5 + 32;
+    }
+
+    if (unit === "celsius") {
+    return (
+        <div className="TempConversion">
+            <span className="temp-degrees"> {Math.round(props.celsius)} </span>
+            <span className="unit"> °C | <a href="/" onClick={showFahrenheit}> °F </a>
+            </span>
         </div>
     );
+    } else {
+        return (
+        <div className="TempConversion">
+            <span className="temp-degrees"> {Math.round(fahrenheit())} </span>
+            <span className="unit"> <a href="/" onClick={showCelsius}>°C </a> |  °F
+            </span>
+        </div>
+    );
+    }
 }
